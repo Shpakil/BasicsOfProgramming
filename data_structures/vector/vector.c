396b8cf27659610a92a8de3a69f589fb0c563280
shpakil <kirillkotenev32@gmail.com>
2024-03-03 15:56:06 +0300

    "append access functions"

--- data_structures/vector/vector.c
+++ data_structures/vector/vector.c
@@ -79,3 +79,27 @@ void popBack(vector *v){
     }
     v->size--;
 }
+
+int* atVector(vector *v, size_t index){
+    if(index>=v->size){
+        fprintf(stderr, "IndexError: a[%zu] is not exists",index);
+        exit(1);
+    }
+    return &v->data[index];
+}
+
+int* back(vector *v) {
+    if (v->size == 0) {
+        fprintf(stderr, "vector is empty");
+        exit(1);
+    }
+    return &v->data[v->size - 1];
+}
+
+int* front(vector *v) {
+    if (v->size == 0) {
+        fprintf(stderr, "vector is empty");
+        exit(1);
+    }
+    return &v->data[0];
+}
--- main.c
+++ main.c
@@ -29,7 +29,48 @@ void test_popBack_notEmptyVector() {
     assert(v.size == 0);
     assert(v.capacity == 1);
 }
+void test_atVector_notEmptyVector() {
+    vector v = createVector(3);
+    pushBack(&v, 10);
+    pushBack(&v, 20);
+    pushBack(&v, 30);
+    int *element = atVector(&v, 1);
+    assert(*element == 20);
+}
+
+void test_atVector_requestToLastElement() {
+    vector v = createVector(3);
+    pushBack(&v, 10);
+    pushBack(&v, 20);
+    pushBack(&v, 30);
+    int *element = atVector(&v, 2);
+    assert(*element == 30);
+
+    // Проверка на доступ к последнему элементу, который должен быть равен последнему добавленному
+    element = atVector(&v, v.size - 1);
+    assert(*element == v.data[v.size - 1]);
+}
+
+void test_back_oneElementInVector() {
+    vector v = createVector(1);
+    pushBack(&v, 10);
+    int *last_element = back(&v);
+    assert(*last_element == 10);
+}
+
+void test_front_oneElementInVector() {
+    vector v = createVector(1);
+    pushBack(&v, 10);
+    int *first_element = front(&v);
+    assert(*first_element == 10);
+}
+
+
 int main() {
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
     test_pushBack_emptyVector();
     test_pushBack_fullVector();
     test_popBack_notEmptyVector();
